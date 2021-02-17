63fbd706aa91e2abce709a0f9492da3bd37103f6
Sam Hall <mann.samantha11@gmail.com>
2021-02-17 13:43:37 -0500

    controller changes

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,5 +10,19 @@ class FavoritesController < ApplicationController
         render json: favorite
     end
 
+    def create
+        Favorite.create(favorite_params)
+        render json: favorite
+    end
+
+    def destroy
+        favorite = Favorite.find_by(id: params['id'])
+        favorite.delete
+        render json: favorite
+    end
+
+    def favorite_params
+        params.require(:favorite).permit(:user_id, :comic_id)
+    end
 
 end
--- app/serializers/comic_serializer.rb
+++ app/serializers/comic_serializer.rb
@@ -1,4 +1,5 @@
 class ComicSerializer < ActiveModel::Serializer
   attributes :id, :name, :image, :bio, :video, :tour_date
   has_many :reviews
+  has_many :favorites
 end
