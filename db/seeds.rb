607ebf50fa85fe724d13723a22958b7c7225a564
Sam Hall <mann.samantha11@gmail.com>
2021-02-19 12:42:42 -0500

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,14 +30,14 @@ comedy_genre: surreal)
 
 silverman = Comic.create(name:"Sarah Silverman", 
 image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Sarah_Silverman_DNC_July_2016.jpg/220px-Sarah_Silverman_DNC_July_2016.jpg", 
-bio: "an American stand-up comedian whose comedy addresses social taboos and controversial topics, such as racism, sexism, politics, and religion, sometimes having her comic character endorse them in a satirical or deadpan fashion.", 
+bio: "Sarah Silverman is an American stand-up comedian whose comedy addresses social taboos and controversial topics, such as racism, sexism, politics, and religion, sometimes having her comic character endorse them in a satirical or deadpan fashion.", 
 video: "https://www.youtube.com/embed/2aRj7RsJxV0", 
 tour_date: "none",
 comedy_genre: blue)
 
 mulaney = Comic.create(name: "John Mulaney", 
 image: "https://upload.wikimedia.org/wikipedia/commons/2/2f/John_Mulaney_PaleyFest_crop.jpg",
-bio: "an American stand-up comedian, actor, writer, and producer. He is best known for his work as a writer on Saturday Night Live and observational, surrealist stand-up specials.", 
+bio: "John Mulaney an American stand-up comedian, actor, writer, and producer. He is best known for his work as a writer on Saturday Night Live and observational, surrealist stand-up specials.", 
 video: "https://www.youtube.com/embed/1MBjJMMKASQ", 
 tour_date: "none",
 comedy_genre: observational)
@@ -65,21 +65,21 @@ comedy_genre: topical)
 
 demetri_martin = Comic.create(name: "Demetri Martin", 
 image: "https://m.media-amazon.com/images/M/MV5BMjA3Nzg5MzM2Nl5BMl5BanBnXkFtZTcwODYzOTAzMg@@._V1_UY1200_CR165,0,630,1200_AL_.jpg",
-bio: "Demetri Evan Martin is an American comedian known for his deadpan delivery, playing his guitar for jokes, and his satirical cartoons.", 
+bio: "Demetri Martin is an American comedian known for his deadpan delivery, playing his guitar for jokes, and his satirical cartoons.", 
 video: "https://www.youtube.com/embed/DsMPMST47G4", 
 tour_date: "none",
 comedy_genre: surreal)
 
 bamford = Comic.create(name: "Maria Bamford", 
 image: "https://bostonglobe-prod.cdn.arcpublishing.com/resizer/2VaBfXczLH7CrsstsIJFoX4yCBA=/1440x0/arc-anglerfish-arc2-prod-bostonglobe.s3.amazonaws.com/public/I7HW7IXEDUI6PNLETXOMBR5OMY.jpg",
-bio: "an American stand-up comedian best known for her portrayal of her dysfunctional family and self-deprecating comedy involving jokes about depression and anxiety.",
+bio: "Maria Bamford is an American stand-up comedian best known for her portrayal of her dysfunctional family and self-deprecating comedy involving jokes about depression and anxiety.",
 video: "https://www.youtube.com/embed/XHxCr0ZkSeU", 
 tour_date: "none",
 comedy_genre: character)
 
 kroll = Comic.create(name: "Nick Kroll", 
 image: "https://media.gq.com/photos/59b2ade979b42439531347ac/master/w_2052,h_3000,c_limit/Nick-Kroll-justin-bettman-04.jpg",
-bio: "Nicholas Kroll is an American comedian known for creating and starring in the Comedy Central series Kroll Show, The Oh, Hello Show, the FX comedy series The League, and co-creating in the Netflix series Big Mouth", 
+bio: "Nick Kroll is an American comedian known for creating and starring in the Comedy Central series Kroll Show, The Oh, Hello Show, the FX comedy series The League, and co-creating in the Netflix series Big Mouth", 
 video: "https://www.youtube.com/embed/McF3Y47AIys", 
 tour_date: "none",
 comedy_genre: character)
@@ -114,7 +114,7 @@ comedy_genre: anti)
 
 iglesias = Comic.create(name: "Gabriel Iglesias",
 image: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Gabriel_Iglesias_111.jpg_-_5176944953_%28cropped%29.jpg",
-bio: "Gabriel Jesús Iglesias, also known as Fluffy, is an American comedian and actor. He is known for his shows I'm Not Fat… I'm Fluffy and Hot & Fluffy",
+bio: "Gabriel Iglesias, also known as Fluffy, is an American comedian and actor. He is known for his shows I'm Not Fat… I'm Fluffy and Hot & Fluffy",
 video: "https://www.youtube.com/embed/azaSaWAyQE4",
 tour_date: "none",
 comedy_genre: topical)
@@ -153,6 +153,50 @@ bio: "Weird Al Yankovic is an American musician who is known for humorous songs 
 video: "https://www.youtube.com/embed/t2mU6USTBRE",
 tour_date: "none",
 comedy_genre: spoof)
+
+steve_martin = Comic.create(name: "Steve Martin",
+image:"https://i.etsystatic.com/5684897/r/il/f8cafa/886667144/il_570xN.886667144_1ytl.jpg",
+bio: "Steve Martin is an American actor, comedian, writer, playwright, producer, and musician. He has earned five Grammy Awards, a Primetime Emmy Award, and has received the Mark Twain Prize for American Humor.",
+video: "https://www.youtube.com/embed/RRocmX0pF9U",
+tour_date: "none",
+comedy_genre: anti)
+
+brady = Comic.create(name: "Wayne Brady",
+image: "https://upload.wikimedia.org/wikipedia/commons/b/bb/Wayne_Brady_2015.jpg",
+bio: "Wayne Brady is an American actor, singer, comedian, game show host, and television personality. He is a regular on Whose Line Is It Anyway?",
+video: "https://www.youtube.com/embed/EPkmlhg8r3k",
+tour_date: "none",
+comedy_genre: comedy_music)
+
+peele = Comic.create(name: "Jordan Peele",
+image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Jordan_Peele_Peabody_2014_%28cropped%29.jpg/440px-Jordan_Peele_Peabody_2014_%28cropped%29.jpg",
+bio: "Jordan Peele is an American actor, comedian and Oscar-winning filmmaker. He is best known for his television and film work in the comedy and horror genres.",
+video: "https://www.youtube.com/embed/yLVdqGv-tsk",
+tour_date: "none",
+comedy_genre: sketch)
+
+carlin = Comic.create(name: "George Carlin",
+image: "https://upload.wikimedia.org/wikipedia/commons/8/8e/George_Carlin_1975_%28Little_David_Records%29_Publicity.jpg",
+bio: "George Carlin was an American stand-up comedian, actor, social critic and author. He's regarded as one of the most important and influential stand-up comics of all time",
+video: "https://www.youtube.com/embed/sdYWQ475aEE",
+tour_date: "none",
+comedy_genre: observational)
+
+hedberg = Comic.create(name: "Mitch Hedberg",
+image: "https://pyxis.nymag.com/v1/imgs/45d/29b/c165ca2af9d4bc7124962abf79cdbefe22-14-mitch-hedberg.rsquare.w1200.jpg",
+bio: "Mitch Hedberg was an American stand-up comedian known for his surreal humor and deadpan delivery.",
+video: "https://www.youtube.com/embed/ax0MGlIVjiY",
+tour_date: "none",
+comedy_genre: surreal)
+
+brooks = Comic.create(name: "Mel Brooks",
+image: "https://cdn.britannica.com/24/200924-050-3B616A9C/Mel-Brooks-guest-actor-comedy-series-Emmy-1998.jpg",
+bio: "Mel Brooks is an American director, writer, actor, comedian, producer and composer. He is known as a creator of broad film farces and comedic parodies.",
+video: "https://www.youtube.com/embed/ZHT07L8iv7I",
+tour_date: "none",
+comedy_genre: spoof)
+
+
 
 
 
